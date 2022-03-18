f2ab6c1ae2a1922e9f569ac77deff816e2e03c7a
ybaspinar <y.baspinar@yandex.com>
2022-03-18 17:12:13 +0300

    added list function

--- main.go
+++ main.go
@@ -1,5 +1,46 @@
-package homework_2_week_3_ybaspinar
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+type Books struct {
+	Bookid      int    `json:"bookid"`
+	Booktitle   string `json:"booktitle"`
+	Pages       int    `json:"pages"`
+	Stockamount int    `json:"stockamount"`
+	Price       int    `json:"price"`
+	Stockid     int    `json:"stockid"`
+	Sbn         int    `json:"ısbn"`
+	Author      struct {
+		Authorid   int    `json:"authorid"`
+		Authorname string `json:"authorname"`
+	} `json:"author"`
+}
+
+// List Prints all the books from the
+func List(Books []Books) {
+	for i := 0; i < len(Books); i++ {
+		fmt.Printf("Book %d : %s \n", i+1, Books[i].Booktitle)
+	}
+}
 
 func main() {
-
+	var Books []Books
+	//Reads JSON file
+	file, err := ioutil.ReadFile("books.json")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	//Decode JSON file to array
+	err = json.Unmarshal(file, &Books)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	List(Books)
 }
